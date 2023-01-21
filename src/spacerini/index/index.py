8a92995fb5b89cc19c8dd3359a0913932a99919d
Christopher Akiki <christopher.akiki@protonmail.com>
2023-01-21 16:13:08 +0100

    Update indexing

--- src/spacerini/index/index.py
+++ src/spacerini/index/index.py
@@ -1,4 +1,11 @@
-def index_json_collection(shards_path, index_path, keep_shards):
+from pyserini.index.lucene import LuceneIndexer
+from pyserini.pyclass import autoclass
+from datasets import load_dataset
+from tqdm import tqdm
+
+def index_json_shards(shards_path, index_path, keep_shards):
+    JIndexCollection = autoclass('io.anserini.index.IndexCollection')
+    JIndexCollection.main()
 # !python -m pyserini.index.lucene \
 #   --collection JsonCollection \
 #   --input to_index_json/ \
@@ -6,11 +13,10 @@ def index_json_collection(shards_path, index_path, keep_shards):
 #   --generator DefaultLuceneDocumentGenerator \
 #   --threads 20 \
 #   --storePositions --storeDocvectors
-    pass
 
-def index_dataframe(df):
-    pass
-
-def index_streaming():
-    pass
-
+def index_streaming_hf_dataset(ds_path, split, index_path, column_to_index, num_rows=None, disable_tqdm=False):
+    ds = load_dataset(ds_path, split=split, streaming=True)
+    indexer = LuceneIndexer(index_path)
+    for i, row in tqdm(enumerate(ds), total=num_rows, disable=disable_tqdm):
+        indexer.add({"id": i,"contents": row[column_to_index]})
+    indexer.close()
