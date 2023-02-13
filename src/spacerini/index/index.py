210cc484d9fc02250d9f23e9ace7e1b341a4d4d6
theyorubayesian <akin.o.oladipo@gmail.com>
2023-02-13 15:33:06 -0500

    update args for on-the-fly indexing

--- src/spacerini/index/index.py
+++ src/spacerini/index/index.py
@@ -1,3 +1,7 @@
+from itertools import chain
+from typing import List
+from typing import Literal
+
 import shutil
 from pyserini.index.lucene import LuceneIndexer
 from pyserini.pyclass import autoclass
@@ -6,72 +10,111 @@ from tqdm import tqdm
 import json
 import os
 
-def index_json_shards(
-    shards_path,
-    index_path,
-    fields=[],
-    language=None,
-    pretokenized=False,
-    hf_tokenizer=None,
-    store_positions=True,
-    store_docvectors=False,
-    store_contents=False,
-    store_raw=False,
-    optimize=False,
-    keep_shards=True,
-    verbose=False,
-    quiet=False,
-    memory_buffer="4096",
-    n_threads=-1,
-    ):
-# LEFT TO ADD:
-# "-keepStopwords"
-# "-stopwords"
-# "-stemmer"
+
+def parse_args(
+    index_path: str,
+    shards_path: str = "",
+    fields: List[str] = None,
+    language: str =None,
+    pretokenized: bool = False,
+    analyzeWithHuggingFaceTokenizer: str = None,
+    storePositions: bool = True,
+    storeDocvectors: bool = False,
+    storeContents: bool = False,
+    storeRaw: bool = False,
+    keepStopwords: bool = False,
+    stopwords: str = None,
+    stemmer:  Literal["porter", "krovetz"] = None,
+    optimize: bool = False,
+    verbose: bool = False,
+    quiet: bool = False,
+    memory_buffer: str ="4096",
+    n_threads: int = -1,
+    **kwargs
+):
+    params = locals()
     args = []
-    args.extend(["-input", shards_path,
-                "-threads", f"{n_threads}" if n_threads!=-1 else f"{os.cpu_count()}",
-                "-collection", "JsonCollection",
-                "-generator", "DefaultLuceneDocumentGenerator",
-                "-index", index_path,
-                "-memorybuffer", memory_buffer])
-    if pretokenized:
-        args.append("-pretokenized")
-    if hf_tokenizer:
-        args.extend(["-analyzeWithHuggingFaceTokenizer", hf_tokenizer])
-    if store_positions:
-        args.append("-storePositions")
-    if store_docvectors:
-        args.append("-storeDocvectors")
-    if store_contents:
-        args.append("-storeContents")
-    if store_raw:
-        args.append("-storeRaw")
-    if optimize:
-        args.append("-optimize")
-    if verbose:
-        args.append("-verbose")
-    if quiet:
-        args.append("-quiet")
+    args.extend([
+        "-input", shards_path,
+        "-threads", f"{n_threads}" if n_threads!=-1 else f"{os.cpu_count()}",
+        "-collection", "JsonCollection",
+        "-generator", "DefaultLuceneDocumentGenerator",
+        "-index", index_path,
+        "-memorybuffer", memory_buffer,
+    ])
+    variables = [
+        "analyzeWithHuggingFaceTokenizer", "language", "stemmer"
+    ]
+    additional_args = [[f"-{var}", params[var]] for var in variables if params[var]]
+    args.extend(chain.from_iterable(additional_args))
+
     if fields:
         args.extend(["-fields", " ".join(fields)])
-    if language:
-        args.extend(["-language", language])
+
+    flags = [
+        "pretokenized", "storePositions", "storeDocvectors", 
+        "storeContents", "storeRaw", "optimize", "verbose", "quiet"
+    ]
+    args.extend([f"-{flag}" for flag in flags if params[flag]])
+    
+    return args
+
+
+def index_json_shards(
+    shards_path: str,
+    index_path: str,
+    fields: List[str] = None,
+    language: str = None,
+    pretokenized: bool = False,
+    analyzeWithHuggingFaceTokenizer: bool = None,
+    storePositions: bool = True,
+    storeDocvectors: bool = False,
+    storeContents: bool = False,
+    storeRaw: bool = False,
+    keepStopwords: bool = False,
+    stopwords: str = None,
+    stemmer:  Literal["porter", "krovetz"] = None,
+    optimize: bool = False,
+    verbose: bool = False,
+    quiet: bool = False,
+    memory_buffer: str = "4096",
+    n_threads: bool = -1,
+    keep_shards: bool = True
+):
+    args = parse_args(**locals())
     JIndexCollection = autoclass('io.anserini.index.IndexCollection')
     JIndexCollection.main(args)
     if not keep_shards:
         shutil.rmtree(shards_path)
 
+
 def index_streaming_hf_dataset(
-    ds_path,
-    split,
-    index_path,
-    column_to_index,
-    num_rows=None,
-    disable_tqdm=False
-    ):
-    ds = load_dataset(ds_path, split=split, streaming=True)
-    indexer = LuceneIndexer(index_path)
+    ds_path: str,
+    split: str,
+    index_path: str,
+    column_to_index: str,
+    ds_language_code: str = None,   # For HF Dataset
+    language: str = None,
+    pretokenized: bool = False,
+    analyzeWithHuggingFaceTokenizer: bool = None,
+    storePositions: bool = True,
+    storeDocvectors: bool = False,
+    storeContents: bool = False,
+    storeRaw: bool = False,
+    keepStopwords: bool = False,
+    stopwords: str = None,
+    stemmer:  Literal["porter", "krovetz"] = None,
+    optimize: bool = False,
+    verbose: bool = False,
+    quiet: bool = False,
+    memory_buffer: str = "4096",
+    n_threads: bool = -1,
+    num_rows: int = None,
+    disable_tqdm: bool = False
+):
+    args = parse_args(**locals())
+    ds = load_dataset(ds_path, name=ds_language_code, split=split, streaming=True)
+    indexer = LuceneIndexer(args)
     for i, row in tqdm(enumerate(ds), total=num_rows, disable=disable_tqdm):
-        indexer.add(json.dumps({"id": i,"contents": row[column_to_index]}))
+        indexer.add(json.dumps({"id": i, "contents": row[column_to_index]}))
     indexer.close()
